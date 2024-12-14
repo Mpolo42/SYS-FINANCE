d38e5a9c83884067207446dae737a9fdf3788244
unknown <celioneto0332@digitalcollege.internal>
2024-12-14 14:52:18 -0300

    Ajuste

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,15 @@
+export default function LoginPage() {
+    return (
+      <div className="w-full h-screen bg-[#1e1e1e] text-white">
+       <div className="w-[50%] h-full justify-items-start">
+
+       </div>
+
+       <div className="w-[50%] h-full justify-items-end">
+
+       </div>
+        
+      </div>
+    )
+}
+  
